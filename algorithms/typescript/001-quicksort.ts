ee1b9615841e4ab186e2581a168c6917588e38fc
Ricardo Carvalho <rat.carvalho@gmail.com>
2022-06-10 10:24:52 -0300

    Resolving a merged file

--- algorithms/typescript/001-quicksort.ts
+++ algorithms/typescript/001-quicksort.ts
@@ -1,39 +1,9 @@
-<<<<<<< HEAD
-const part = (a: number[]): [number[], number[]] => {
-  const pivot = a.length - 1;
-  let j = 0;
-
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] < a[pivot]) {
-      [a[i], a[j]] = [a[j], a[i]];
-      j++;
-    }
-  }
-
-  [a[j], a[pivot]] = [a[pivot], a[j]];
-
-  return [a.splice(0, j), a.splice(1)];
-};
-
-const quicksort = (a: number[]): number[] => {
-  if (a.length > 1) {
-    let [p1, p2] = part(a);
-
-    let r1 = quicksort(p1);
-    let r2 = quicksort(p2);
-
-    let r = [...r1, a[0], ...r2];
-    return r;
-  }
-=======
 const quicksort = (a: number[]): number[] => {
   if (a.length > 1) {
     const p1 = a.slice(0, a.length - 1).filter((x) => x < a[a.length - 1]);
     const p2 = a.slice(0, a.length - 1).filter((x) => x >= a[a.length - 1]);
     return [...quicksort(p1), a[a.length - 1], ...quicksort(p2)];
   }
-
->>>>>>> f5ace1ffc3454fa9b64d8d60a049f19654fbe565
   return a;
 };
 
